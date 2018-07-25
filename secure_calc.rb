cae19f2e6a8094cc28ee2518760caa4ba5e86f9c
Nicci Sheets <niccisheets@aol.com>
2018-07-25 06:42:21 -0400

    need to work on dividing by zero result

--- secure_calc.rb
+++ secure_calc.rb
@@ -9,23 +9,19 @@ def work(operator, n1, n2)
 		result = n1.to_f * n2.to_f
 	when "divide"
 		if n2 == 0
-			result1 = "Cannot Divide By Zero - Solution Is Undefined"
+			result = "Cannot Divide By Zero - Solution Is Undefined"
 		else
 			result = n1.to_f / n2.to_f
 		end
 	end
-	result || result1
+	result
 	if result.to_s.split("").last.to_i == 0
 		result = result.to_s.split("").values_at(0..-3).join.to_i
-	elsif		
+	else
 		result = result.round(2)
-	elsif 
-		result1		
 	end
-	result = " = #{result}" 
-	result1
-	
+	result = " = #{result}"
 end
 
 
-p work("divide", 30, 30)
+#p work("divide", 30, 0)
