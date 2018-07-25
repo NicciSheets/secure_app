f2d9b89ef0b9dda1a8abf3eed11712fa27a14c5a
Nicci Sheets <niccisheets@aol.com>
2018-07-25 04:21:30 -0400

    css box color

--- secure_calc.rb
+++ secure_calc.rb
@@ -8,20 +8,24 @@ def work(operator, n1, n2)
 	when "multiply"
 		result = n1.to_f * n2.to_f
 	when "divide"
-		if n2.to_f == 0.0
-			result = "Cannot Divide By Zero - Solution Is Undefined"
-		elsif 
+		if n2 == 0
+			result1 = "Cannot Divide By Zero - Solution Is Undefined"
+		else
 			result = n1.to_f / n2.to_f
 		end
 	end
-	result
+	result || result1
 	if result.to_s.split("").last.to_i == 0
 		result = result.to_s.split("").values_at(0..-3).join.to_i
-	else
+	elsif		
 		result = result.round(2)
+	elsif 
+		result1		
 	end
-	" = #{result}"
+	result = " = #{result}" 
+	result1
+	
 end
 
 
-#work("add", 2.3, 3)
+p work("divide", 30, 30)
