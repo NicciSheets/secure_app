b61e13146eb97a142fcf00d5b1c130b5069319fd
Nicci Sheets <niccisheets@aol.com>
2018-07-25 10:52:37 -0400

    for real math

--- secure_calc.rb
+++ secure_calc.rb
@@ -3,9 +3,6 @@ def work(operator, n1, n2)
 	case operator
 	when "add"
 		result = n1.to_f + n2.to_f
-		if result.to_s.split("").last.to_i == 0 
-			result = result.to_s.split("").values_at(0..-3).join.to_i
-		end
 	when "subtract"
 		result = n1.to_f - n2.to_f
 		if result.to_s.split("").last.to_i == 0 
@@ -13,26 +10,25 @@ def work(operator, n1, n2)
 		end
 	when "multiply"
 		result = n1.to_f * n2.to_f
-		if result.to_s.split("").last.to_i == 0 
-			result = result.to_s.split("").values_at(0..-3).join.to_i
-		end
 	when "divide"
 		if n2 == 0
 			result = "Cannot Divide By Zero - Solution Is Undefined"
 		else
 			result = n1.to_f / n2.to_f
-			if result.to_s.split("").last.to_i == 0 
-				result = result.to_s.split("").values_at(0..-3).join.to_i
-			end
 		end
 	end
 	result
-		unless result == "Cannot Divide By Zero - Solution Is Undefined"
+		if result == "Cannot Divide By Zero - Solution Is Undefined" 
+			result 
+		elsif result.to_s.split("").last.to_i == 0 
+			result = result.to_s.split("").values_at(0..-3).join.to_i
+		else 
 			result = result.round(2)
 		end
+
 	result = " = #{result}"
-		
+
 end
 
 
-p work("divide", 30.3, 5)
+p work("divide", 20, 6)
